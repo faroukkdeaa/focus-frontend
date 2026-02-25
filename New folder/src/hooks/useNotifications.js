95d7a823e34e9e16a42b62aa02ec028ba912c42e
faroukkdeaa <amdf1@fci.suezuni.edu.eg>
2026-02-25 05:21:10 +0200

    Delete New folder directory

--- New folder/src/hooks/useNotifications.js
+++
@@ -1,8 +0,0 @@
-import { useContext } from 'react';
-import { NotificationContext_internal } from '../context/NotificationContext';
-
-export const useNotifications = () => {
-  const ctx = useContext(NotificationContext_internal);
-  if (!ctx) throw new Error('useNotifications must be used inside <NotificationProvider>');
-  return ctx;
-};
